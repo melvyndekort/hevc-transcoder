26182b0382ffb1decafa91c894acbf7a78830731
Melvyn de Kort <m.de.kort@portbase.com>
2023-03-02 15:19:04 +0100

    Remove origin file if target file already exists

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+get_duration() {
+  ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$1" | cut -d. -f1
+}
+
 rsync -a /nextcloud/melvyndekort/files/InstantUpload/Camera/ /photos/melvyn/
 rsync -a /nextcloud/kaatjeislief/files/InstantUpload/Camera/ /photos/karin/
 rsync -a /nextcloud/daandekort/files/InstantUpload/Camera/ /photos/daan/
@@ -9,20 +13,30 @@ for INPUT in /photos/**/*.mp4; do
   [[ "$INPUT" = *"-hevc.mp4" ]] && continue
 
   OUTPUT="$(echo $INPUT | sed 's/.mp4$/-hevc.mp4/')"
-  [ -f "$OUTPUT" ] && continue
+
+  if [ -f "$OUTPUT" ]; then
+    INDUR="$(get_duration """$INPUT""")"
+    OUTDUR="$(get_duration """$OUTPUT""")"
+
+    case "$(($INDUR-$OUTDUR))" in
+      -1) rm -f "$INPUT"; continue ;;
+      0)  rm -f "$INPUT"; continue ;;
+      1)  rm -f "$INPUT"; continue ;;
+      *)  rm -f "$OUTPUT"; echo "WARN: $INPUT seemed to be incorrect, re-encoding" ;;
+    esac
+  fi
 
   echo "Starting conversion: $INPUT -> $OUTPUT"
   ffmpeg -i "$INPUT" -c:v libx265 -crf 26 -preset fast -c:a copy "$OUTPUT"
   [ "$?" -ne 0 ] && rm -f "$OUTPUT" && echo "Something went wrong" && exit 1
   touch -r "$INPUT" "$OUTPUT"
 
-  INDUR="$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 """$INPUT""" | cut -d. -f1)"
-  OUTDUR="$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 """$OUTPUT""" | cut -d. -f1)"
+  INDUR="$(get_duration """$INPUT""")"
+  OUTDUR="$(get_duration """$OUTPUT""")"
   case "$(($INDUR-$OUTDUR))" in
-    -1) rm -f "$INPUT"; echo "Finished conversion: $INPUT -> $OUTPUT" ;;
-    0)  rm -f "$INPUT"; echo "Finished conversion: $INPUT -> $OUTPUT" ;;
-    1)  rm -f "$INPUT"; echo "Finished conversion: $INPUT -> $OUTPUT" ;;
+    -1) rm -f "$INPUT"; echo "Finished conversion: $INPUT -> $OUTPUT"; continue ;;
+    0)  rm -f "$INPUT"; echo "Finished conversion: $INPUT -> $OUTPUT"; continue ;;
+    1)  rm -f "$INPUT"; echo "Finished conversion: $INPUT -> $OUTPUT"; continue ;;
     *)  rm -f "$OUTPUT"; echo "ERROR: Something went wrong!"; exit 1 ;;
   esac
-
 done
