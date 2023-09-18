01eb0078ec28cdaf15c4707d1f0a67e37e965bdd
Melvyn de Kort <melvyn@mdekort.nl>
2023-09-19 00:11:48 +0200

    Fully implemented container, but no logging because of IPv6 only networking

--- terraform/fargate.tf
+++ terraform/fargate.tf
@@ -5,18 +5,34 @@ resource "aws_ecs_task_definition" "hevc_encoder" {
   cpu                      = 2048
   memory                   = 4096
   execution_role_arn       = aws_iam_role.hevc_fargate.arn
-  
+
   container_definitions = jsonencode([
     {
       name      = "hevc-encoder"
-      image     = "melvyndekort/hevc-encoder:latest"
-      cpu       = 2048
-      memory    = 4096
+      image     = "registry.ipv6.docker.com/melvyndekort/hevc-encoder:latest"
       essential = true
     }
   ])
-  
+
   runtime_platform {
     cpu_architecture = "ARM64"
+  }
+}
+
+resource "aws_security_group" "hevc_encoder" {
+  name        = "hevc-encoder"
+  description = "hevc-encoder"
+  vpc_id      = data.terraform_remote_state.cloudsetup.outputs.vpc_id
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "hevc-encoder"
   }
 }
