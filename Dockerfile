ebb7864b096b9ec49f598ab80d65c8289a38c796
Melvyn de Kort <melvyn@mdekort.nl>
2023-09-18 17:42:03 +0200

    Simple container

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,7 @@
 FROM alpine:latest
 
-RUN apk add --update --no-cache ffmpeg x265 bash rsync
+RUN apk add --update --no-cache ffmpeg x265 aws-cli
 
 COPY run.sh /
 
-VOLUME /nextcloud
-VOLUME /photos
-
-CMD [ "/bin/bash", "/run.sh" ]
+CMD [ "/bin/sh", "-c", "/run.sh" ]
--- run.sh
+++ run.sh
@@ -1,44 +1,5 @@
-#!/bin/bash
+#!/bin/sh
 
-get_duration() {
-  DUR="$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 """$1""")"
-  [ "$?" -ne 0 ] && DUR=0
-  echo "$DUR" | cut -d. -f1
-}
+set -xe
 
-rsync -a /nextcloud/melvyndekort/files/InstantUpload/Camera/ /photos/melvyn/
-rsync -a /nextcloud/kaatjeislief/files/InstantUpload/Camera/ /photos/karin/
-rsync -a /nextcloud/daandekort/files/InstantUpload/Camera/ /photos/daan/
-
-shopt -s globstar
-for INPUT in /photos/**/*.mp4; do
-  [[ "$INPUT" = *"-hevc.mp4" ]] && continue
-
-  OUTPUT="$(echo $INPUT | sed 's/.mp4$/-hevc.mp4/')"
-
-  if [ -f "$OUTPUT" ]; then
-    INDUR="$(get_duration """$INPUT""")"
-    OUTDUR="$(get_duration """$OUTPUT""")"
-
-    case "$(($INDUR-$OUTDUR))" in
-      -1) rm -f "$INPUT"; continue ;;
-      0)  rm -f "$INPUT"; continue ;;
-      1)  rm -f "$INPUT"; continue ;;
-      *)  rm -f "$OUTPUT"; echo "WARN: $INPUT seemed to be incorrect, re-encoding" ;;
-    esac
-  fi
-
-  echo "Starting conversion: $INPUT -> $OUTPUT"
-  ffmpeg -i "$INPUT" -c:v libx265 -crf 26 -preset fast -c:a copy "$OUTPUT"
-  [ "$?" -ne 0 ] && rm -f "$OUTPUT" && echo "Something went wrong" && exit 1
-  touch -r "$INPUT" "$OUTPUT"
-
-  INDUR="$(get_duration """$INPUT""")"
-  OUTDUR="$(get_duration """$OUTPUT""")"
-  case "$(($INDUR-$OUTDUR))" in
-    -1) rm -f "$INPUT"; echo "Finished conversion: $INPUT -> $OUTPUT"; continue ;;
-    0)  rm -f "$INPUT"; echo "Finished conversion: $INPUT -> $OUTPUT"; continue ;;
-    1)  rm -f "$INPUT"; echo "Finished conversion: $INPUT -> $OUTPUT"; continue ;;
-    *)  rm -f "$OUTPUT"; echo "ERROR: Something went wrong!"; exit 1 ;;
-  esac
-done
+env
