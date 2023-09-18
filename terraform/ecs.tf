658b53452ae3e10ff06bd035fdaf1c6ba43b31ce
Melvyn de Kort <melvyn@mdekort.nl>
2023-09-18 13:37:07 +0200

    Add terraform setup for automation

---
+++ terraform/ecs.tf
@@ -0,0 +1,20 @@
+resource "aws_cloudwatch_log_group" "hevc" {
+  name = "hevc"
+}
+
+resource "aws_ecs_cluster" "hevc" {
+  name = "hevc"
+  /*
+  configuration {
+    execute_command_configuration {
+      kms_key_id = aws_kms_key.example.arn
+      logging    = "OVERRIDE"
+
+      log_configuration {
+        cloud_watch_encryption_enabled = true
+        cloud_watch_log_group_name     = aws_cloudwatch_log_group.example.name
+      }
+    }
+  }
+*/
+}
---
+++ terraform/eventbridge.tf
@@ -0,0 +1,66 @@
+resource "aws_cloudwatch_event_rule" "s3_upload_mp4" {
+  name        = "capture-mp4-upload-events"
+  description = "Capture all upload events of mp4 files in an S3 bucket"
+
+  event_pattern = jsonencode({
+    source      = ["aws.s3"],
+    detail-type = ["AWS API Call via CloudTrail"],
+    detail = {
+      eventSource = ["s3.amazonaws.com"],
+      eventName   = ["PutObject"],
+      requestParameters = {
+        bucketName = [aws_s3_bucket.hevc.id],
+        key = [
+          {
+            prefix = "TODO/"
+          },
+          {
+            suffix = [".mp4", ".MP4"]
+          }
+        ]
+      }
+    }
+  })
+}
+
+resource "aws_cloudwatch_event_target" "fargate_hevc_encoder" {
+  target_id = "fargate-hevc-encoder"
+  rule      = aws_cloudwatch_event_rule.s3_upload_mp4.name
+  arn       = aws_ecs_cluster.hevc.arn
+  role_arn  = aws_iam_role.hevc_fargate.arn
+
+  ecs_target {
+    task_count          = 1
+    task_definition_arn = aws_ecs_task_definition.hevc_encoder.arn
+    launch_type         = "FARGATE"
+  }
+
+  input_transformer {
+    input_paths = {
+      bucketname      = "$.detail.bucket.name",
+      objectkey       = "$.detail.object.key",
+      objectversionid = "$.detail.object.version-id",
+    }
+    input_template = jsonencode({
+      containerOverrides = [
+        {
+          name = "hevc-encoder"
+          environment = [
+            {
+              name  = "S3_BUCKET_NAME"
+              value = "bucketname"
+            },
+            {
+              name  = "S3_OBJECT_KEY"
+              value = "objectkey"
+            },
+            {
+              name  = "S3_OBJ_VERSION_ID"
+              value = "objectversionid"
+            }
+          ]
+        }
+      ]
+    })
+  }
+}
