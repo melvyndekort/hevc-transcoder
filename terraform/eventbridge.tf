aeefbeed946f8ef7dddc5a7c4d2629d81d13a757
Melvyn de Kort <melvyn@mdekort.nl>
2023-09-20 19:21:53 +0200

    Format terraform files

--- terraform/eventbridge.tf
+++ terraform/eventbridge.tf
@@ -3,14 +3,14 @@ resource "aws_cloudwatch_event_rule" "s3_upload_mp4" {
   description = "Capture all upload events of mp4 files in an S3 bucket"
 
   event_pattern = jsonencode({
-    source      = [ "aws.s3" ],
-    detail-type = [ "Object Created" ],
+    source      = ["aws.s3"],
+    detail-type = ["Object Created"],
     detail = {
       bucket = {
-        name = [ aws_s3_bucket.hevc.id ]
+        name = [aws_s3_bucket.hevc.id]
       }
       object = {
-        key = [ { prefix = "TODO/" } ]
+        key = [{ prefix = "TODO/" }]
       }
     }
   })
