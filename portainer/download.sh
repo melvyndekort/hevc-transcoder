a9635c1a95b04a63d17f5717336b0073826c70a8
Melvyn de Kort <melvyn@mdekort.nl>
2023-09-21 19:53:09 +0200

    Add scripts to instrument Portainer

---
+++ portainer/download.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+set -e
+
+# Get the endpoint id
+echo "# Get the endpoint id"
+ENDPOINT_ID=$(http --verify false \
+  GET \
+  $HOST/api/endpoints \
+  X-API-Key:$PORTAINER_API_KEY | \
+  jq -r '.[] | select(.Name=="lmserver").Id'
+)
+echo "ENDPOINT_ID = $ENDPOINT_ID"
+
+# Pull the image
+echo "# Pulling the image"
+http --verify false \
+  POST \
+  $HOST/api/endpoints/$ENDPOINT_ID/docker/images/create \
+  Content-Type:application/json \
+  X-API-Key:$PORTAINER_API_KEY \
+  fromImage==melvyndekort/hevc-download \
+  tag==latest \
+  -p m
+
+# Create the container
+echo "# Creating the container"
+CONTAINER_ID=$(http --verify false \
+  POST \
+  $HOST/api/endpoints/$ENDPOINT_ID/docker/containers/create?name=hevc-download \
+  Content-Type:application/json \
+  X-API-Key:$PORTAINER_API_KEY \
+  Image=melvyndekort/hevc-download:latest \
+  Env[]=AWS_REGION=eu-west-1 \
+  Env[]=AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+  Env[]=AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+  HostConfig[AutoRemove]:=true \
+  HostConfig[Mounts][0][Target]=/data \
+  HostConfig[Mounts][0][Source]=/var/mnt/storage/photos \
+  HostConfig[Mounts][0][Type]=bind \
+  HostConfig[Mounts][0][ReadOnly]:=false | \
+  jq -r '.Id'
+)
+echo "CONTAINER_ID = $CONTAINER_ID"
+
+# Start the container
+echo "# Starting the container"
+http --verify false \
+  POST \
+  $HOST/api/endpoints/$ENDPOINT_ID/docker/containers/$CONTAINER_ID/start \
+  Content-Type:application/json \
+  X-API-Key:$PORTAINER_API_KEY \
+  --raw='{}' \
+  -p m
+
+echo "Container $CONTAINER_ID started"
