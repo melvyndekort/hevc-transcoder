53d072ba682baa9ee6d3bd851037f97e3689ac8b
Melvyn de Kort <melvyn@mdekort.nl>
2023-09-21 20:32:19 +0200

    Working docker containers

--- docker/hevc-download/run.sh
+++ docker/hevc-download/run.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
-echo TODO
+set -e
+
+aws s3api list-objects-v2 --no-cli-pager --bucket mdekort.hevc --prefix DONE --query "Contents[].Key" --output yaml | \
+sed 's#^- ##' | \
+while read FILE; do
+  TARGET="$(echo $FILE | sed 's#DONE/##')"
+  aws s3api get-object --no-cli-pager --bucket mdekort.hevc --key "$FILE" "/data/$TARGET"
+  aws s3api delete-object --no-cli-pager --bucket mdekort.hevc --key "$FILE"
+
+  ORIGINAL="$(echo $TARGET | sed 's/-hevc.mp4$/.mp4/')"
+  if [[ -f "/data/$ORIGINAL" ]]; then
+    rm -f "/data/$ORIGINAL"
+  fi
+done
