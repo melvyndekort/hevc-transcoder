eb47840075d69f0c5c05c9f26b082536d3a3e937
Melvyn de Kort <melvyn@mdekort.nl>
2023-09-21 20:32:19 +0200

    Working docker containers

--- docker/hevc-upload/run.sh
+++ docker/hevc-upload/run.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-echo TODO
+find /data -name '*.mp4' ! -name '*-hevc.mp4' | while read FILE; do
+  NEWFILE="$(echo $FILE | sed 's/.mp4$/-hevc.mp4/')"
+  aws s3api head-object --no-cli-pager --bucket mdekort.hevc --key DONE/$NEWFILE && continue
+  aws s3api head-object --no-cli-pager --bucket mdekort.hevc --key TODO/$FILE && continue
+  aws s3api put-object --no-cli-pager --bucket mdekort.hevc --key TODO/$FILE --body $FILE
+done
