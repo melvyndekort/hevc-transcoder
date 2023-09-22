bcb4ba5b9ac83258bcfacebf9c2664a99d5d0f75
Melvyn de Kort <melvyn@mdekort.nl>
2023-09-22 18:25:24 +0200

    Fix bug in paths

--- docker/hevc-portainer/download.sh
+++ docker/hevc-portainer/download.sh
@@ -2,15 +2,14 @@
 
 set -xe
 
-aws s3api list-objects-v2 --no-cli-pager --bucket mdekort.hevc --prefix DONE --query "Contents[].Key" --output yaml | \
-sed 's#^- ##' | \
-while read FILE; do
-  TARGET="$(echo $FILE | sed 's#DONE/##')"
-  aws s3api get-object --no-cli-pager --bucket mdekort.hevc --key "$FILE" "/data/$TARGET"
-  aws s3api delete-object --no-cli-pager --bucket mdekort.hevc --key "$FILE"
+aws s3api list-objects-v2 --no-cli-pager --bucket mdekort.hevc --prefix DONE --query "Contents[].Key" --output text | tr '\t' '\n' | \
+while read KEY; do
+  TARGET="$(echo $KEY | sed 's#^DONE/##')"
+  aws s3api get-object --no-cli-pager --bucket mdekort.hevc --key "$KEY" "$TARGET"
+  aws s3api delete-object --no-cli-pager --bucket mdekort.hevc --key "$KEY"
 
   ORIGINAL="$(echo $TARGET | sed 's/-hevc.mp4$/.mp4/')"
-  if [[ -f "/data/$ORIGINAL" ]]; then
-    rm -f "/data/$ORIGINAL"
+  if [[ -f "$ORIGINAL" ]]; then
+    rm -f "$ORIGINAL"
   fi
 done
--- docker/hevc-portainer/upload.sh
+++ docker/hevc-portainer/upload.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-find /data -name '*.mp4' ! -name '*-hevc.mp4' | while read FILE; do
-  NEWFILE="$(echo $FILE | sed 's/.mp4$/-hevc.mp4/')"
-  aws s3api head-object --no-cli-pager --bucket mdekort.hevc --key DONE/$NEWFILE && continue
-  aws s3api head-object --no-cli-pager --bucket mdekort.hevc --key TODO/$FILE && continue
-  aws s3api put-object --no-cli-pager --bucket mdekort.hevc --key TODO/$FILE --body $FILE
+find . -name '*.mp4' ! -name '*-hevc.mp4' | while read FILE; do
+  KEY="$(echo $FILE | sed 's/^.\///')"
+  DONE_KEY="$(echo $KEY | sed 's/.mp4$/-hevc.mp4/')"
+  aws s3api head-object --no-cli-pager --bucket mdekort.hevc --key DONE/$DONE_KEY && continue
+  aws s3api head-object --no-cli-pager --bucket mdekort.hevc --key TODO/$KEY && continue
+  aws s3api put-object --no-cli-pager --bucket mdekort.hevc --key TODO/$KEY --body $FILE
 done
