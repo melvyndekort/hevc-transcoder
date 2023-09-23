5480a8bb8209076320f3e17c6d822205e6e259cd
Melvyn de Kort <melvyn@mdekort.nl>
2023-09-23 12:05:35 +0200

    Don't upload files which are already converted

--- docker/hevc-portainer/upload.sh
+++ docker/hevc-portainer/upload.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 find . -name '*.mp4' ! -name '*-hevc.mp4' | while read FILE; do
+  DONE_FILE="$(echo $FILE | sed 's/.mp4$/-hevc.mp4/')"
+  if [[ -f "$DONE_FILE" ]]; then
+    echo rm -f "$FILE"
+    continue
+  fi
   KEY="$(echo $FILE | sed 's/^.\///')"
   DONE_KEY="$(echo $KEY | sed 's/.mp4$/-hevc.mp4/')"
   aws s3api head-object --no-cli-pager --bucket mdekort.hevc --key DONE/$DONE_KEY && continue
