06167e1b604de9305f19cb078bf0550b09647d05
Melvyn de Kort <melvyn@mdekort.nl>
2023-09-23 20:56:43 +0200

    Fix bug and remove empty directories after sync

--- docker/hevc-portainer/nextcloud-sync.sh
+++ docker/hevc-portainer/nextcloud-sync.sh
@@ -2,9 +2,13 @@
 
 set -e
 
-rsync --dry-run -a --remove-from-source /source/melvyndekort/files/InstantUpload/Camera/ /target/Smartphones/melvyn/
-rsync --dry-run -a --remove-from-source /source/kaatjeislief/files/InstantUpload/Camera/ /target/Smartphones/karin/
-rsync --dry-run -a --remove-from-source /source/daandekort/files/InstantUpload/Camera/   /target/Smartphones/daan/
+rsync --dry-run -a --remove-source-files /source/melvyndekort/files/InstantUpload/Camera/ /target/Smartphones/melvyn/
+rsync --dry-run -a --remove-source-files /source/kaatjeislief/files/InstantUpload/Camera/ /target/Smartphones/karin/
+rsync --dry-run -a --remove-source-files /source/daandekort/files/InstantUpload/Camera/   /target/Smartphones/daan/
+
+find /source/melvyndekort/files -type d -empty -delete
+find /source/kaatjeislief/files -type d -empty -delete
+find /source/daandekort/files   -type d -empty -delete
 
 chown -R root:root /target
 find /target -type d -exec chmod 755 {} \;
