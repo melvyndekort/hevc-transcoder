032ea49e8688ab2f56fb9f06b9abaf7db356507d
Melvyn de Kort <melvyn@mdekort.nl>
2023-10-24 21:22:56 +0200

    Add testcase for main function

--- docker/hevc-portainer/tests/processor/test_processor.py
+++ docker/hevc-portainer/tests/processor/test_processor.py
@@ -35,3 +35,41 @@ def test_delete_done_files(bucket, basedir):
   assert basedir + '/todo.mp4' in file_list
   assert basedir + '/subdir/done-hevc.mp4' in file_list
   assert basedir + '/subdir/todo.mp4' in file_list
+
+def test_main(monkeypatch):
+  from processor import processor
+
+  class MockVideoFile():
+    uploaded = False
+    upload_counter = 0
+    done_counter = 0
+    download_counter = 0
+
+    def upload_for_processing(self):
+      self.upload_counter += 1
+      self.uploaded = True
+
+    def is_done(self):
+      if self.done_counter == 0:
+        self.done_counter += 1
+        return False
+      else:
+        self.done_counter += 1
+        return True
+
+    def download_processed(self):
+      self.download_counter += 1
+
+  mock_video = MockVideoFile()
+
+  def mock_list_videos(basedir):
+    return [mock_video]
+
+  monkeypatch.setattr(processor, "list_videos", mock_list_videos)
+  monkeypatch.setattr(processor, "SLEEP", 0.001)
+
+  processor.main(None)
+
+  assert mock_video.upload_counter == 1
+  assert mock_video.done_counter == 2
+  assert mock_video.download_counter == 1
