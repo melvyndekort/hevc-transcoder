6cc0003031c2715f74814b3f5512a33b4f70f5e5
Melvyn de Kort <melvyn@mdekort.nl>
2023-10-27 11:02:13 +0200

    Refactored processor

--- hevc-portainer/processor/processor.py
+++ hevc-portainer/processor/processor.py
@@ -12,26 +12,39 @@ SLEEP = 60
 
 from processor.video_file import VideoFile
 
+def delete_original(filename):
+    original_name = filename.replace('-hevc.mp4', '.mp4')
+    original = Path(original_name)
+    if original.is_file():
+        logger.info(f'Deleting local file {original_name} from filesystem')
+        original.unlink()
+
+def add_file(videos, basedir, filename):
+    converted = filename.replace('.mp4', '-hevc.mp4')
+    if not Path(converted).is_file():
+        relpath = filename.removeprefix(basedir + '/')
+        video_file = VideoFile(basedir, relpath)
+        videos.append(video_file)
+
 def list_videos(basedir):
     videos = []
 
     for file in Path(basedir).rglob("*.mp4"):
         filename = str(file)
         if filename.endswith('-hevc.mp4'):
-            # delete original file when it has already been converted
-            original_name = filename.replace('-hevc.mp4', '.mp4')
-            original = Path(original_name)
-            if original.is_file():
-                logger.info(f'Deleting local file {original_name} from filesystem')
-                original.unlink()
+            delete_original(filename)
         else:
-            converted = filename.replace('.mp4', '-hevc.mp4')
-            if not Path(converted).is_file():
-                relpath = filename.removeprefix(basedir + '/')
-                video_file = VideoFile(basedir, relpath)
-                videos.append(video_file)
+            add_file(videos, basedir, filename)
 
     return videos
+
+def process_videos(videos):
+    for video in videos[:]:
+        if not video.uploaded:
+            video.upload_for_processing()
+        elif video.is_done():
+            video.download_processed()
+            videos.remove(video)
 
 def main(basedir):
     logger.info('Start processing')
@@ -39,12 +52,7 @@ def main(basedir):
     videos = list_videos(basedir)
 
     while len(videos) > 0:
-        for video in videos[:]:
-            if not video.uploaded:
-                video.upload_for_processing()
-            elif video.is_done():
-                video.download_processed()
-                videos.remove(video)
+        process_videos(videos)
         time.sleep(SLEEP)
 
     logger.info('Finished processing')
