792b4f5313676e3691e636c16b0cabd97120f20b
Melvyn de Kort <melvyn@mdekort.nl>
2023-11-29 12:47:18 +0100

    Use an actual video file in manual tests and disable logging and public ip

--- scripts/manual-run.sh
+++ scripts/manual-run.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 
+VIDEO=/tmp/video-$$.mp4
+curl -sLo $VIDEO https://sample-videos.com/video123/mp4/480/big_buck_bunny_480p_2mb.mp4
+aws s3 cp $VIDEO s3://mdekort.hevc/temp.mp4
+rm -f $VIDEO
+
 aws ecs run-task \
 	--no-cli-pager \
-	--task-definition $(aws ecs list-task-definitions --no-cli-pager --query 'taskDefinitionArns[0]') \
+	--task-definition $(aws ecs list-task-definitions --no-cli-pager --query 'taskDefinitionArns[*]' --output text) \
 	--network-configuration "$(cat <<-EOF
 {
 	"awsvpcConfiguration":{
 		"subnets": $(aws ec2 describe-subnets --no-cli-pager --filter 'Name=tag:Name,Values=generic-public-*' --query 'Subnets[*].SubnetId'),
 		"securityGroups": $(aws ec2 describe-security-groups --no-cli-pager --filter 'Name=group-name,Values=hevc-transcoder' --query 'SecurityGroups[*].GroupId'),
-		"assignPublicIp": "ENABLED"
+		"assignPublicIp": "DISABLED"
 	}
 }
 EOF
@@ -25,7 +30,7 @@ EOF
 				},
 				{
 					"name":"S3_OBJECT_KEY",
-					"value":"TODO/temp.mp4"
+					"value":"temp.mp4"
 				}
 			]
 		}
