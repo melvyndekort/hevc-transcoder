a5e0342dea18e0d6e8b00b1ca8be9c35ecb0d1fa
Melvyn de Kort <melvyn@mdekort.nl>
2024-07-10 18:03:46 +0200

    Exit after 4 hours, JSON logging, code formatted

---
+++ hevc_transcoder/logging_config.py
@@ -0,0 +1,29 @@
+import logging.config
+from pythonjsonlogger import jsonlogger
+
+LOGGING_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "json": {
+            "format": "%(asctime)s %(levelname)s %(name)s %(message)s",
+            "datefmt": "%Y-%m-%dT%H:%M:%SZ",
+            "class": "pythonjsonlogger.jsonlogger.JsonFormatter",
+        }
+    },
+    "handlers": {
+        "stdout": {
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",
+            "formatter": "json",
+        }
+    },
+    "loggers": {
+      "": {
+        "handlers": ["stdout"],
+        "level": "INFO"
+      },
+    },
+}
+
+logging.config.dictConfig(LOGGING_CONFIG)
